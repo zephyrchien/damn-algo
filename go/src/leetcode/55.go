c4a079c0665b7f73dfcc864e60f91f576f21e4d6
zephyr <i@zephyr.moe>
2024-06-19 00:50:20 +0900

    lc

---
+++ go/src/leetcode/55.go
@@ -0,0 +1,15 @@
+package main
+
+func canJump(nums []int) bool {
+	dp := make([]bool, len(nums))
+	dp[0] = true
+	for i := 0; i < len(nums); i++ {
+		if !dp[i] {
+			continue
+		}
+		for j := 1; j <= nums[i] && i+j < len(nums); j++ {
+			dp[i+j] = true
+		}
+	}
+	return dp[len(nums)-1]
+}
