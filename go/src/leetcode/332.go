b916a018192e447e3a37d0b0c20276dfb3107881
zephyr <i@zephyr.moe>
2024-06-18 21:13:50 +0900

    lc 332.go

---
+++ go/src/leetcode/332.go
@@ -0,0 +1,23 @@
+package main
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		var cnt = -1
+		for _, c := range coins {
+			if i >= c && dp[i-c] >= 0 {
+				if cnt < 0 {
+					cnt = dp[i-c]
+				} else {
+					cnt = min(cnt, dp[i-c])
+				}
+			}
+		}
+		if cnt >= 0 {
+			dp[i] = cnt + 1
+		} else {
+			dp[i] = -1
+		}
+	}
+	return dp[amount]
+}
